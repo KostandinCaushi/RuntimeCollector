dd309d1ce7bac60174bc1c0a2e081ee4e74a3a74
Kostandin Caushi <koci4@live.it>
2019-11-09 16:59:23 +0100

    [PayloadRequest] added SharedPreferences map

--- app/src/main/java/com/kostandin/caushi/runtimecollector/service/PayloadRequest.java
+++ app/src/main/java/com/kostandin/caushi/runtimecollector/service/PayloadRequest.java
@@ -1,6 +1,8 @@
 package com.kostandin.caushi.runtimecollector.service;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PayloadRequest {
 
@@ -8,6 +10,9 @@ public class PayloadRequest {
     // Device Info
     private String deviceInfo;
     private String uiInfo;
+
+    // Preferences
+    private Map<String, ?> preferences;
 
     // Performance
     private String cpuUsage;
@@ -22,12 +27,16 @@ public class PayloadRequest {
     private HashMap<String, String> objectValuesMap;
     private HashMap<String, String> intentsMap;
 
-    // Maps fot Methods and UI
+    // UI
     private HashMap<String, String> viewMap;
-        // Contains also touchMethods
-    private HashMap<String, String> methodsMap;
-    private HashMap<String, String> touchMethodsMap;
-    private HashMap<String, String> methodsAndViewMap;
+    private List<String> viewFlow;
+
+    private List<String> methodsAndViewFlow;
+
+    // Methods
+    private HashMap<String, HashMap<String, String>> methodsMap;         // Contains also touchMethods
+    private HashMap<String, HashMap<String, String>> touchMethodsMap;
+
 
 
     public String getDeviceInfo() {
@@ -44,6 +53,14 @@ public class PayloadRequest {
 
     public void setUiInfo(String uiInfo) {
         this.uiInfo = uiInfo;
+    }
+
+    public Map<String, ?> getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(Map<String, ?> preferences) {
+        this.preferences = preferences;
     }
 
     public String getCpuUsage() {
@@ -110,27 +127,35 @@ public class PayloadRequest {
         this.viewMap = viewMap;
     }
 
-    public HashMap<String, String> getMethodsMap() {
+    public HashMap<String, HashMap<String, String>> getMethodsMap() {
         return methodsMap;
     }
 
-    public void setMethodsMap(HashMap<String, String> methodsMap) {
+    public void setMethodsMap(HashMap<String, HashMap<String, String>> methodsMap) {
         this.methodsMap = methodsMap;
     }
 
-    public HashMap<String, String> getTouchMethodsMap() {
+    public HashMap<String, HashMap<String, String>> getTouchMethodsMap() {
         return touchMethodsMap;
     }
 
-    public void setTouchMethodsMap(HashMap<String, String> touchMethodsMap) {
+    public void setTouchMethodsMap(HashMap<String, HashMap<String, String>> touchMethodsMap) {
         this.touchMethodsMap = touchMethodsMap;
     }
 
-    public HashMap<String, String> getMethodsAndViewMap() {
-        return methodsAndViewMap;
+    public List<String> getViewFlow() {
+        return viewFlow;
     }
 
-    public void setMethodsAndViewMap(HashMap<String, String> methodsAndViewMap) {
-        this.methodsAndViewMap = methodsAndViewMap;
+    public void setViewFlow(List<String> viewFlow) {
+        this.viewFlow = viewFlow;
+    }
+
+    public List<String> getMethodsAndViewFlow() {
+        return methodsAndViewFlow;
+    }
+
+    public void setMethodsAndViewFlow(List<String> methodsAndViewFlow) {
+        this.methodsAndViewFlow = methodsAndViewFlow;
     }
 }
